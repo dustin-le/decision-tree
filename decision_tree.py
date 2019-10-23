379d73c9f5f9dd2f46292f6482866e5e375b686d
Dustin Le <ledustin2@gmail.com>
2019-10-23 04:45:07 -0500

    Implemented skeleton.

---
+++ decision_tree.py
@@ -0,0 +1,19 @@
+# Dustin Le
+# 1001130689
+
+from sys import argv
+import numpy as np
+
+def decision_tree(training_file, test_file, option, pruning_thr):
+    train = []
+    test = []
+
+    with open(training_file, 'r') as training:
+        for line in training:
+            train.append(line.split())
+    
+    with open(test_file, 'r') as testing:
+        for line in testing:
+            test.append(line.split())
+
+    
